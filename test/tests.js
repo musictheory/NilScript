c01c36d4b9f993e9ca752a3626fef4d804607c2c
Ricci Adams <git@ricciadams.com>
2013-02-04 16:17:09 -0800

    Checking in before modifying Modifier

---
+++ test/tests.js
@@ -0,0 +1,29 @@
+//@opts = { }
+
+var ojc = require("../src/compiler");
+var fs  = require("fs");
+var assert = require("assert");
+
+
+var compile = function(inFile, options) {
+    var inContent = "";
+
+    inContent += fs.readFileSync(__dirname + "/../src/runtime.js");
+    inContent += fs.readFileSync(inFile, "utf8");
+
+    return ojc.compile(inContent, options);
+}
+
+
+function runTest(name)
+{
+    var src = compile(__dirname + "/inc/" + name + ".oj");
+
+    test(name, function() {
+        assert(eval(src), true);
+    });
+}
+
+runTest("IvarAndPropertiesClass");
+runTest("InheritanceSuite");
+runTest("MultilineMethodClass");
