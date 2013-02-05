5bb051efd501e7f4e2c1b4204c684fdb552a6de4
Ricci Adams <git@ricciadams.com>
2013-02-04 21:59:13 -0800

    New tests, things compile and actually work! :)

--- src/modifier.js
+++ src/modifier.js
@@ -1,102 +1,177 @@
 /* Based on Modifier from harmonizr (http://github.com/jdiamond/harmonizr) */
 
-var esprima = require("esprima");
-var util = require("util");
+var Modifier = (function() {
 
-var Modifier = (function () {
-    function _clone(loc) {
-        return {line: loc.line, column:loc.column};
-    }
 
-    function Modifier(src) {
-        this.ast = esprima.parse(src, { loc: true });
-        this.lines = src.split('\n');
-        this.replacements = [ ];
-    }
-    Modifier.prototype.finish = function() {
-        var replacement, from, line;
+function _clone(loc)
+{
+    return {line: loc.line, column:loc.column};
+}
 
-        this.replacements.sort(function(a, b) {
-            if (a.lineIndex == b.lineIndex) {
-                return b.toColumn - a.toColumn;
-            } else {
-                return b.lineIndex - a.lineIndex;
+
+function _isDescendantOf(a, b)
+{
+    var keys   = Object.keys(b);
+    var result = false;
+
+    for (var i = 0, length = keys.length; i < length; i++) {
+        var child = b[keys[i]];
+
+        if (child && typeof child === "object") {
+            if (a === child || _isDescendantOf(a, child)) {
+                result = true;
+                break;
             }
-        });
-
-        for (var i = 0, length = this.replacements.length; i < length; i++) {
-            replacement = this.replacements[i];
-            line = this.lines[replacement.lineIndex];          
-            this.lines[replacement.lineIndex] = line.substring(0, replacement.fromColumn) + (replacement.text || "") + line.substring(replacement.toColumn);
         }
-
-        this.replacements = [ ];
-
-        return this.lines.join('\n');
-    };
-    Modifier.prototype.refresh = function() {
-        this.ast = parse(this.finish(), { loc: true });
-    };
-    /**
-     * Removes all text between `from` and `to`.
-     * Removal is *inclusive* which means the char pointed to by `to` is removed
-     * as well. This makes it possible to remove a complete expression by
-     * calling remove(expr.start, expr.end);
-     * `to` may also be the number of characters to remove
-     */
-    Modifier.prototype._addReplacement = function(line, fromColumn, toColumn, text) {
-        if (fromColumn === undefined) fromColumn = 0;
-        if (toColumn   === undefined) toColumn = this.lines[line - 1].length;
-
-        var existing = this.lines[line - 1].substring(fromColumn, toColumn);
-        var replacement = {lineIndex: (line - 1), fromColumn: fromColumn, toColumn: toColumn};
-        if (text) replacement.text = text;
-        if (existing) replacement.existing = existing;
-        this.replacements.push(replacement);
     }
 
-    Modifier.prototype._replace = function(from, to, text) {
-        // Make copy of from, as we may be passing in objects in the AST
-        from = _clone(from);
+    return result;
+}
 
-        if (to === null || to === undefined) {
-            to = {line: from.line, column: this.lines[from.line - 1].length }
-        } else if (typeof to === 'number') {
-            to = {line: from.line, column: from.column + to};
+
+function Modifier(src, options)
+{
+    this._lines0 = src.split('\n');
+    this._current = { };
+    this._replacements = [ ];
+    this._debug = (options && options.debug);
+}
+
+
+Modifier.prototype._doReplacements = function()
+{
+    this._replacements.sort(function(a, b) {
+        if (a.line == b.line) {
+            return b.toColumn - a.toColumn;
         } else {
-            to = _clone(to);
+            return b.line - a.line;
         }
+    });
 
-        if (from.line != to.line) {
-            // Remove segment of first line
-            this._addReplacement(from.line, from.column);
+    for (var i = 0, length = this._replacements.length; i < length; i++) {
+        var r      = this._replacements[i];
+        var line   = this._getLine(r.line);          
+        var before = line.substring(0, r.fromColumn);
+        var after  = line.substring(r.toColumn);
 
-            // Remove intermediate lines completely
-            for (var lineno = from.line + 1; lineno < to.line - 1; lineno++) {
-                this._addReplacement(lineno, 0);
+        if (this._debug) {
+            var toRemove = line.substring(r.fromColumn, r.toColumn);
+
+            if (r.text && toRemove) {
+                console.log("" + r.line + ": replacing '" + toRemove + "' with '" + r.text +"'");
+            } else if (toRemove) {
+                console.log("" + r.line + ": deleting '" + toRemove + "'");
+            } else if (r.text) {
+                console.log("" + r.line + ": inserting '" + r.text + "'");
             }
-
-            // Replace segment of last line with replacement text
-            this._addReplacement(to.line, 0, to.column, text);
-        } else {
-            this._addReplacement(to.line, from.column, to.column, text);
         }
-    };
-    Modifier.prototype.remove = function(from, to, inclusive) {
-        this._replace(from, to);
-    };
-    Modifier.prototype.insert = function(pos, text) {
-        this._addReplacement(pos.line, pos.column, pos.column, text);
-    };
-    Modifier.prototype.replace = function(from, to, text, inclusive) {
-        this._replace(from, to, text);
+        this._setLine(r.line, before + (r.text || "") + after);
     }
-; return Modifier;})();
+
+    this._replacements = [ ];
+}
+
+
+Modifier.prototype._getLine = function(line1)
+{
+    return this._lines0[line1 - 1];
+}
+
+
+Modifier.prototype._setLine = function(line1, text)
+{
+    this._lines0[line1 - 1] = text;        
+}
+
+
+Modifier.prototype._addReplacement = function(line, fromColumn, toColumn, text)
+{
+    if ((fromColumn == toColumn) && !text) {
+        return;
+    }
+
+    var replacement = { line: line, fromColumn: fromColumn, toColumn: toColumn };
+    if (text) replacement.text = text;
+    this._replacements.push(replacement);
+}
+
+
+
+Modifier.prototype._flush = function()
+{
+    var c = this._current;
+    var text = c.text;
+    var start, end, replaceAtStart;
+
+    if (c.from && c.to) {
+        if (_isDescendantOf(c.to, c.from)) {
+            start = c.from.loc.start;
+            end   = c.to.loc.start;
+            replaceAtStart = true;
+
+        } else if (_isDescendantOf(c.from, c.to)) {
+            start = c.from.loc.end;
+            end   = c.to.loc.end;
+
+        } else {
+            start = c.from.loc.start;
+            end   = c.to.loc.end;
+        }
+
+    } else if (c.select) {
+        start = c.select.loc.start;
+        end   = c.select.loc.end;
+
+    } else if (c.before) {
+        start = end = c.before.loc.start;
+
+    } else if (c.after) {
+        start = end = c.after.loc.end;
+    }
+
+    if (start.line != end.line) {
+        this._addReplacement(start.line, start.column, this._getLine(start.line).length, replaceAtStart ? text : "");
+
+        // Remove intermediate lines completely
+        for (var line = start.line + 1; line < end.line; line++) {
+            this._addReplacement(line, 0, this._getLine(line).length, "");
+        }
+
+        this._addReplacement(end.line, 0, end.column, replaceAtStart ? "" : text);
+
+    } else {
+        this._addReplacement(start.line, start.column, end.column, text);
+    }
+
+    this._current = { };
+}
+
+
+Modifier.prototype.from   = function(node) { this._current.from   = node;  return this; }
+Modifier.prototype.to     = function(node) { this._current.to     = node;  return this; }
+Modifier.prototype.select = function(node) { this._current.select = node;  return this; }
+Modifier.prototype.before = function(node) { this._current.before = node;  return this; }
+Modifier.prototype.after  = function(node) { this._current.after  = node;  return this; }
+
+Modifier.prototype.remove  = 
+Modifier.prototype.insert  = 
+Modifier.prototype.replace = function(text)
+{
+    this._current.text = text;
+    this._flush();
+}
+
+
+Modifier.prototype.finish = function()
+{
+    this._doReplacements();
+    return this._lines0.join('\n');
+}
+
+return Modifier;
+
+})();
 
 var Modifier = Modifier;
-/* vim: set sw=4 ts=4 et tw=80 : */
 
-
-module.exports = {
-    Modifier: Modifier
-};
+module.exports = { Modifier: Modifier };
