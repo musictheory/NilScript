5fad984a8f1e9ecd85927c0ef6db895e15d68d4c
Ricci <git@ricciadams.com>
2015-11-27 18:43:28 -0800

    Readds source mapping support

---
+++ src/mapper.js
@@ -0,0 +1,54 @@
+/*
+    mapper.js
+    Wraps SourceMapGenerator to generate source maps
+    (c) 2013-2015 musictheory.net, LLC
+    MIT license, http://www.opensource.org/licenses/mit-license.php
+*/
+
+"use strict";
+
+const SourceMapGenerator = require && require("source-map").SourceMapGenerator;
+
+
+module.exports = class Mapper {
+
+constructor(files, sourceRoot)
+{
+    this._generator = new SourceMapGenerator({ 
+        file:       files,
+        sourceRoot: sourceRoot
+    });
+
+    this._outLine = 1;
+}
+
+
+add(name, lines)
+{
+    if (!lines) {
+        console.log(name, lines);
+        return;
+    }
+
+    if (name) {
+        for (let i = 1, length = lines.length; i <= length; i++) {
+            this._generator.addMapping({
+                source: name,
+                original:  { line: i,               column: 0 },
+                generated: { line: this._outLine++, column: 0 }
+            });
+        }
+
+    } else {
+        this._outLine += lines.length;
+    }
+}
+
+
+getSourceMap()
+{
+    return this._generator.toString();
+}
+
+
+}
